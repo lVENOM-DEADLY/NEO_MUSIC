818d8e953241c0c669bb2f3dba88d0d16c6ff793
lVENOM-DEADLY <shivampatel425685@gmail.com>
2025-09-15 06:17:08 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -52,7 +52,7 @@ EXTRA_PLUGINS = getenv(
 
 EXTRA_PLUGINS_REPO = getenv(
     "EXTRA_PLUGINS_REPO",
-    "https://github.com/THE-VIP-BOY-OP/Extra-Plugin",
+    "https://github.com/TANYA-SINGH-VNS-UP/EXTRA-PLUGIN",
 )
 # Fill here the external plugins repo where plugins that you want to load
 
